3f0cb9b529d80e9b4bcdc7d2f35770f5d409883e
priyankamurthy28 <priyankamurthy28@gmail.com>
2025-09-09 17:27:21 +0530

    Update Contents.swift

--- Pages/Trees.xcplaygroundpage/Contents.swift
+++ Pages/Trees.xcplaygroundpage/Contents.swift
@@ -279,3 +279,28 @@ func isSubTree(root: Node?, subRoot: Node?) -> Bool {
 }
 
 isSubTree(root: root, subRoot: root)
+
+
+func lowestCommonAncestor(_ root: TreeNode?, _ p: TreeNode?, _ q: TreeNode?) -> TreeNode? {
+        guard let root = root else {
+            return nil
+        }
+       if root === p || root === q { return root } 
+
+        let left = lowestCommonAncestor(root.left, p, q)
+        let right = lowestCommonAncestor(root.right, p, q)
+
+        if left == nil {
+            return right
+        }
+        if right == nil {
+            return left
+        }
+
+    return root 
+    }
+}
+
+    
+
+
